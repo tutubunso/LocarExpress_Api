3af206bdbb2ef2a28a422bd1b6162286a7655bbd
tutubunso <aymardbutungane@gmail.com>
2022-05-03 15:34:52 +0200

    views

--- Api/views.py
+++ Api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, mixins
+from rest_framework.views import APIView
+from django.db import transaction
+from .serializers import *
+from .models import *
+from datetime import datetime,timedelta
+from rest_framework.decorators import action
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from django.db.models import Avg
 
-# Create your views here.
+class TokenPairView(TokenObtainPairView):
+    serializer_class = TokenPairSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all().order_by('-id')
+
+class GroupViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
+    permission_classes = IsAuthenticated,
+    queryset = Group.objects.all().order_by('-id')
+    serializer_class = GroupSerializer
