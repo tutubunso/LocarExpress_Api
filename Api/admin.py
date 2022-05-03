e5d4f76f554a9dfa703a8b157ac1b3858af62d4b
tutubunso <aymardbutungane@gmail.com>
2022-05-03 16:54:03 +0200

    adding  urls on admin

--- Api/admin.py
+++ Api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Personnels)
+admin.site.register(Voiture)
+admin.site.register(Carburant)
+admin.site.register(Etat)
+admin.site.register(Document)
+admin.site.register(Chauffeur)
+admin.site.register(Tarif)
+admin.site.register(Location)
